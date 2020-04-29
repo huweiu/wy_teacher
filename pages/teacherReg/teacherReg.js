55342eb3e4df9ee09442a9fce0b4f9ea57c426ab
huweiu <1179813582@qq.com>
2020-04-30 00:50:47 +0800

    小程序代码提交

--- pages/teacherReg/teacherReg.js
+++ pages/teacherReg/teacherReg.js
@@ -1,75 +1,29 @@
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    userimg: '',
-    nickname: '',
+    dates: '1900-01-01',
+    times: '12:00',
+    objectArray: ['中国', '英国', '美国'],
+    index: 0,
   },
-
-  // 点击上传图片
-  upShopLogo: function () {
-    var that = this;
-    wx.showActionSheet({
-      itemList: ['从相册中选择', '拍照'],
-      itemColor: "#f7982a",
-      success: function (res) {
-        if (!res.cancel) {
-          if (res.tapIndex == 0) {
-            that.chooseWxImageShop('album');//从相册中选择
-          } else if (res.tapIndex == 1) {
-            that.chooseWxImageShop('camera');//手机拍照
-          }
-        }
-      }
+  //  点击时间组件确定事件  
+  bindTimeChange: function (e) {
+    console.log("谁哦按")
+    this.setData({
+      times: e.detail.value
     })
   },
-
- // 3.选择图片
-
-chooseWxImageShop: function (type) {
-    var that = this;
-    wx.chooseImage({
-      sizeType: ['original', 'compressed'],
-      sourceType: [type],
-      success: function (res) {
-        that.data.userimg = res.tempFilePaths[0],
-          that.upload_file(urldate.upimg + 'shop/shopIcon', res.tempFilePaths[0])
-        userimg = res.tempFilePaths[0];
-        that.setData({
-          userimg: userimg
-        })
-      }
+  //  点击日期组件确定事件  
+  bindDateChange: function (e) {
+    console.log(e.detail.value)
+    this.setData({
+      dates: e.detail.value
     })
   },
-
- // 4.上传图片到服务器
-upload_file: function (url, filePath) {
-    var that = this;
-    var signature = signa.signaturetik('token=' + token, 'userAccessToken=' + userAccessToken, 'studentAccessToken=' + studentAccessToken);
-    wx.uploadFile({
-      url: urldate.upimg,//后台处理接口
-      filePath: filePath,
-      name: 'file',
-      header: {
-        'content-type': 'multipart/form-data'
-      }, // 设置请求的 header
-      formData: {//需要的参数
-        'token': token,
-        'signature': signature,
-        'userAccessToken': userAccessToken,
-        'studentAccessToken': studentAccessToken
-      }, // HTTP 请求中其他额外的 form data
-      success: function (res) {
-        var data = JSON.parse(res.data);
-        that.setData({
-          userimg: data.path,
-        });
-        that.showMessage('上传成功');
-      },
-      fail: function (res) {
-      }
+  //  点击城市组件确定事件  
+  bindPickerChange: function (e) {
+    console.log(e.detail.value)
+    this.setData({
+      index: e.detail.value
     })
-  },  
+  }
 })
